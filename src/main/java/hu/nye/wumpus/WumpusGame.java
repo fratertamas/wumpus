ab7ca67a47265f9417f58cf30f1f717b0bb6b420
fratertamas <fratertomi@gmail.com>
2023-12-03 17:10:57 +0100

    Input: player name

--- src/main/java/hu/nye/wumpus/WumpusGame.java
+++ src/main/java/hu/nye/wumpus/WumpusGame.java
@@ -1,7 +1,17 @@
 package hu.nye.wumpus;
 
+import hu.nye.wumpus.model.Player;
+
+import java.util.Scanner;
+
 public class WumpusGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Player player;
 
+        System.out.print("Kérem a felhasználónevet: ");
+        player = new Player(scanner.nextLine());
+
+        System.out.println("Hello " + player.getPlayerName() + " jó játékot!\n");
     }
 }
