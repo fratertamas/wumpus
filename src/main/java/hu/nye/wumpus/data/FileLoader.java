9d81edcabe3925b613733b9476d4fb0d44728947
fratertamas <fratertomi@gmail.com>
2023-12-03 19:17:01 +0100

    add FileLoader

--- src/main/java/hu/nye/wumpus/data/FileLoader.java
+++ src/main/java/hu/nye/wumpus/data/FileLoader.java
@@ -16,7 +16,18 @@ public class FileLoader {
             throw new RuntimeException(e);
         }
         Scanner scanner = new Scanner(inputStream);
+        // A pálya mérete
+        int size = scanner.nextInt();
 
-        
+        // A hős pozíciója és iránya
+        int heroColumn = scanner.next().charAt(0) - 'A';
+        int heroRow = scanner.nextInt();
+        char heroDirection = scanner.next().charAt(0);
+
+        // A pálya elemei
+        char[][] field = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            field[i] = scanner.next().toCharArray();
+        }
     }
 }
