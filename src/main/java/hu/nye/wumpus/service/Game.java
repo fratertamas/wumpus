ee717f858a8c17383ec582ae67f1a62042e21ff5
fratertamas <fratertomi@gmail.com>
2023-12-05 14:01:04 +0100

    Implement shoot

--- src/main/java/hu/nye/wumpus/service/Game.java
+++ src/main/java/hu/nye/wumpus/service/Game.java
@@ -1,5 +1,6 @@
 package hu.nye.wumpus.service;
 
+import hu.nye.wumpus.model.Arrow;
 import hu.nye.wumpus.model.Board;
 import hu.nye.wumpus.menu.Menu;
 import hu.nye.wumpus.model.Hero;
@@ -54,7 +55,68 @@ public class Game {
 }
 
     private static void shoot() {
-
+        if (hero.getNumberOfArrows() > 0){
+            Arrow arrow = new Arrow(hero.getHeroRow(), hero.getHeroColumn(), hero.getHeroDirection());
+            char field;
+            if (arrow.getArrowDirection() == 'E') {
+                for (int i = arrow.getArrowColumn(); i < board.getSizeOfBoard(); i++) {
+                    field = board.getBoard()[arrow.getArrowRow()-1][i];
+                    if (field == 'W') {
+                        hero.setNumberOfArrows(hero.getNumberOfArrows()-1);
+                        System.out.println("A falat találtad el, elvesztettél egy nyílat!");
+                        printHeroData();
+                        printBoard();
+                    }else if(field == 'U') {
+                        hero.setNumberOfArrows(hero.getNumberOfArrows()-1);
+                        System.out.println("Lelőtted a Wumpust, nyertél!");
+                    }
+                }
+            }else if (arrow.getArrowDirection() == 'W') {
+                for (int i = arrow.getArrowColumn(); i >= 0; i--) {
+                    field = board.getBoard()[arrow.getArrowRow()-1][i];
+                    if (field == 'W') {
+                        hero.setNumberOfArrows(hero.getNumberOfArrows()-1);
+                        System.out.println("A falat találtad el, elvesztettél egy nyílat!");
+                        printHeroData();
+                        printBoard();
+                    }else if(field == 'U') {
+                        hero.setNumberOfArrows(hero.getNumberOfArrows()-1);
+                        System.out.println("Lelőtted a Wumpust, nyertél!");
+                    }
+                }
+            } else if (arrow.getArrowDirection() == 'N') {
+                for (int i = arrow.getArrowRow()-1; i >= 0; i--){
+                    field = board.getBoard()[i][arrow.getArrowColumn()];
+                    System.out.println(field);
+                    if (field == 'W') {
+                        hero.setNumberOfArrows(hero.getNumberOfArrows()-1);
+                        System.out.println("A falat találtad el, elvesztettél egy nyílat!");
+                        printHeroData();
+                        printBoard();
+                    }else if(field == 'U') {
+                        hero.setNumberOfArrows(hero.getNumberOfArrows()-1);
+                        System.out.println("Lelőtted a Wumpust, nyertél!");
+                    }
+                }
+            } else {
+                System.out.println("Nyílak mezői sorban a pozíciótól a végéig:");
+                for (int i = arrow.getArrowRow()-1; i < board.getSizeOfBoard(); i++){
+                    field = board.getBoard()[i][arrow.getArrowColumn()];
+                    System.out.println(field);
+                    if (field == 'W') {
+                        hero.setNumberOfArrows(hero.getNumberOfArrows()-1);
+                        System.out.println("A falat találtad el, elvesztettél egy nyílat!");
+                        printHeroData();
+                        printBoard();
+                    }else if(field == 'U') {
+                        hero.setNumberOfArrows(hero.getNumberOfArrows()-1);
+                        System.out.println("Lelőtted a Wumpust, nyertél!");
+                    }
+                }
+            }
+        }else{
+            System.out.println("Nincs több nyilad, nem lőhetsz!");
+        }
     }
 
     private static void printBoard() {
